c41b166fa50c9dd7f1c3e48cbce8fad7e787c662
sriisrikanth <47024319+sriisrikanth@users.noreply.github.com>
2019-01-26 07:14:29 +0530

    reverse of number
    reversrsion of 123 number

---
+++ src/reverse.rs
@@ -0,0 +1,11 @@
+fn main()
+{
+    let a=123;
+    let b=a/10;
+    let c=a%10;
+    let d=b/10;
+    let e=b%10;
+    let f=d/10;
+    let g=d%10;
+    println!("the reverse number : {}{}{}",c,e,g);
+}
